7416ce71a34a2a38275333f50a73ef861cd3e007
akashmuhammadabrrar <madborakash48@gmail.com>
2025-09-10 16:12:02 +0600

    this commit for the deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  // Enable static HTML export
+  output: "export",
+  // Optional: if deploying to a subfolder, uncomment and set this
+  // basePath: "/your-subfolder",
+  // assetPrefix: "/your-subfolder/",
 };
 
 export default nextConfig;
